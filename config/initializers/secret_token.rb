985705a63b6437962efe1b090c2e702a0ee1d16e
sdilshod <samatov.dilshod@gmail.com>
2014-07-24 23:50:30 +0500

    initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scrabler::Application.config.secret_key_base = '44ad222f6aa23103c032e1ddd85ce1e51c6c9deed7015a0e167dd87ddf52333c4ffcdf0c1bf39a18241f82cefe112625df7b7eeaffa0c562181c951e07f105a0'
